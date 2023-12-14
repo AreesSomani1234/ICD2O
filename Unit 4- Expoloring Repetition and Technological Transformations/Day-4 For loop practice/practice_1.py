bb7af9485f4ba46cd6c734f624c2e32201410ec7
Arees Somani <asomani@bayviewglen.ca>
2023-12-13 20:19:12 -0500

    practice_for_loops

---
+++ Unit 4- Expoloring Repetition and Technological Transformations/Day-4 For loop practice/practice_1.py
@@ -0,0 +1,83 @@
+#1
+# for i in range(10):
+#     print(i + 1)
+
+#2
+# for i in range(2,11,2):
+#     print(i)
+
+#3
+# isum = 0
+# for i in range(1,8,2):
+#     isum = isum + i
+# print(isum)
+
+#4
+# for i in range(10,0,-1):
+#     print(i)
+
+#5
+# for i in range(4,21,4):
+#     print(i)
+
+#6
+# product = 1
+# for i in range(1,6):
+#     product = product*i
+# print(product)
+
+#7
+# for i in range(3,13):
+#     if i % 3 == 0:
+#         print(i)
+    
+#8
+# for i in range(5,0,-1):
+#     print(i)
+# print("Blast off!")
+
+#9
+# squarei = 1
+# for i in range(1,6):
+#     squarei = i**2
+#     print(squarei)
+
+#10
+
+# for n in range(1,8):
+#     product=1
+#     for i in range(n,0,-1):
+#         product=product*i
+#     print(product)
+
+# #11
+# str = "WONDERFUL"
+# for i in range(1,len(str),2):
+#     print(str[i])
+
+
+
+# #13
+# count = 0
+# str = "COMPUTER"
+# list = ["A", "E", "I", "O", "U"]
+# for i in range(0,len(str)):
+#     if str[i] in list:
+#         count = count + 1
+# print(count)
+
+#14
+
+backwards=""
+forwards="RACECAR"
+for i in range(len(forwards)-1,-1,-1):
+    backwards = backwards + forwards[i]
+
+if backwards == forwards:
+    print("yes")
+
+else:
+    print("no")
+
+
+
